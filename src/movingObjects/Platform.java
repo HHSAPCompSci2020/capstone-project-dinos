adecae0d0fc4972162b046e7430230bc3ae7b2b2
Isaac Phoon <iphoon056@student.fuhsd.org>
2021-05-11 23:18:10 -0700

    Added the code for platform

--- src/movingObjects/Platform.java
+++ src/movingObjects/Platform.java
@@ -1,5 +1,12 @@
 package movingObjects;
 
-public class Platform {
+import processing.core.PImage;
+
+public class Platform extends Item {
+
+	public Platform(PImage img, int x, int y, int w, int h) {
+		super(img, x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
 
 }
